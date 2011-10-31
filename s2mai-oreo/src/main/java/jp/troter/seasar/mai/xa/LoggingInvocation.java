ce0a6a03f234ebb1db25aba53f8cf6a813b52641
troter <trot.thunder@gmail.com>
2011-10-31 18:35:04 +0900

    add LoggingXaTransportImpl and LoggingInvocation

---
+++ s2mai-oreo/src/main/java/jp/troter/seasar/mai/xa/LoggingInvocation.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package jp.troter.seasar.mai.xa;
+
+import org.seasar.framework.log.Logger;
+import org.seasar.mai.mail.MailExceptionHandler;
+import org.seasar.mai.mail.SendMail;
+import org.seasar.mai.mail.impl.MailExceptionHandlerImpl;
+import org.seasar.mai.xa.Invocation;
+
+import com.ozacc.mail.Mail;
+import com.ozacc.mail.MailException;
+
+public class LoggingInvocation extends Invocation {
+    private Logger logger = Logger.getLogger(LoggingInvocation.class);
+
+    private MailExceptionHandler mailExceptionHandler = new MailExceptionHandlerImpl();
+
+    private SendMail sendMail;
+
+    private Mail mail;
+
+    public LoggingInvocation(SendMail sendMail, Mail mail) {
+        super(sendMail, mail);
+        this.sendMail = sendMail;
+        this.mail = mail;
+    }
+
+    public void send() {
+        logger.debug("send mail...(xa)");
+        logger.debug(mail);
+        try {
+            sendMail.send(mail);
+        } catch (MailException e) {
+            logger.error("failure send mail!");
+            logger.error(mail);
+            mailExceptionHandler.handle(e);
+        }
+        logger.debug("success send mail.");
+    }
+
+    public void setMailExceptionHandler(MailExceptionHandler mailExceptionHandler) {
+        this.mailExceptionHandler = mailExceptionHandler;
+    }
+
+}
---
+++ s2mai-oreo/src/main/java/jp/troter/seasar/mai/xa/impl/LoggingXaTransportImpl.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package jp.troter.seasar.mai.xa.impl;
+
+import javax.transaction.Transaction;
+import javax.transaction.TransactionManager;
+
+import jp.troter.seasar.mai.xa.LoggingInvocation;
+
+import org.seasar.framework.util.TransactionUtil;
+import org.seasar.mai.mail.SendMail;
+import org.seasar.mai.mail.Transport;
+import org.seasar.mai.util.TransactionManagerUtil;
+import org.seasar.mai.xa.Invocation;
+import org.seasar.mai.xa.MailStack;
+import org.seasar.mai.xa.impl.XAResourceImpl;
+
+import com.ozacc.mail.Mail;
+
+public class LoggingXaTransportImpl implements Transport {
+
+    private TransactionManager transactionManager;
+
+    public void send(Mail mail, SendMail sendMail) {
+        Invocation invocation = new LoggingInvocation(sendMail, mail);
+
+        if (TransactionManagerUtil.hasTransaction(transactionManager)) {
+            MailStack mailStack = getMailStack();
+            if (mailStack != null) {
+                mailStack.push(invocation);
+                return;
+            }
+        }
+        invocation.send();
+    }
+
+    private MailStack getMailStack() {
+        Transaction tx = TransactionManagerUtil.getTransaction(transactionManager);
+        if (tx == null) {
+            return null;
+        }
+        XAResourceImpl resource = new XAResourceImpl();
+        TransactionUtil.enlistResource(tx, resource);
+        return resource.getMailStack();
+    }
+
+    public void setTransactionManager(TransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+}
